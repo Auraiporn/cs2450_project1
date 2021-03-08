9f63ef0f3667d3266d0653a1bab60ef6e239d725
hyin <51980614+hyin8@users.noreply.github.com>
2021-03-07 20:59:52 -0800

    Add border to Sudoku
    Paint in the border using JLayerPane

--- cs2450_project1/src/main/java/team_penguin/cs2450_project1/SudokuTest.java
+++ cs2450_project1/src/main/java/team_penguin/cs2450_project1/SudokuTest.java
@@ -19,12 +19,35 @@ import javax.swing.JComponent;
 import javax.swing.KeyStroke;
 import javax.swing.BorderFactory;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import java.awt.Graphics2D;
 import javax.swing.Timer;
 
 /**
  *
  * @author amirc
  */
+class Sudoku_Border extends JPanel{
+    // 1 for horizontal
+    // 2 for vertical
+    int orientation; 
+    public Sudoku_Border(int ori){
+        orientation = ori;
+    }
+    public void paintComponent(Graphics g)
+    {
+        java.awt.Graphics2D g2D = (java.awt.Graphics2D) g;
+        g2D.setStroke(new BasicStroke(3));
+        if(this.orientation == 1)
+        {
+            g2D.drawLine(0, 0, 302, 0);
+        }
+        else if(this.orientation == 2)
+        {
+            g2D.drawLine(0, 0, 0, 302);
+        }
+    }
+}
 public class SudokuTest extends javax.swing.JFrame {
     
     /**
@@ -103,11 +126,15 @@ public class SudokuTest extends javax.swing.JFrame {
     
     private void generateBoard()
     {
+        javax.swing.JLayeredPane layer = new javax.swing.JLayeredPane();
+        layer.setBounds(150,40,302,302);
+        //layer.setBorder(BorderFactory.createLineBorder(Color.red));
+        
         //Generate jPanel container for all textFields
         javax.swing.JPanel GameBoard = new javax.swing.JPanel();
         GameBoard.setBackground(new java.awt.Color(255, 255, 255));
-        GameBoard.setBounds(150,40,300,300);
-        GameBoard.setBorder(BorderFactory.createLineBorder(Color.darkGray));
+        GameBoard.setBounds(1,1,300,300);
+        //GameBoard.setBorder(BorderFactory.createLineBorder(Color.darkGray));
         
         //Adjust Layout
         GameBoard.setLayout(new FlowLayout(FlowLayout.LEADING,0,0));
@@ -131,8 +158,25 @@ public class SudokuTest extends javax.swing.JFrame {
                     sudoku_board[i][j].setBackground(Color.LIGHT_GRAY);
                 }
             }
+        }        
+        
+        //Draw Boarder
+        int y_position[] = new int []{0,99,199,299};       
+        for(int i =0; i < 4; i++)
+        {
+            Sudoku_Border b = new Sudoku_Border(1);
+            b.setBounds(1, y_position[i], 300, 10);
+            layer.add(b,javax.swing.JLayeredPane.DRAG_LAYER);
         }
-        this.add(GameBoard);
+        for(int i =0; i < 4; i++)
+        {
+            Sudoku_Border b = new Sudoku_Border(2);
+            b.setBounds(y_position[i],1, 10, 300);
+            layer.add(b,javax.swing.JLayeredPane.DRAG_LAYER);
+        }
+        
+        layer.add(GameBoard,javax.swing.JLayeredPane.DEFAULT_LAYER);
+        this.add(layer);
         
     }
     
@@ -188,10 +232,10 @@ public class SudokuTest extends javax.swing.JFrame {
         getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(496, 296, -1, -1));
 
         Date.setText("Date");
-        getContentPane().add(Date, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 20, 110, 23));
+        getContentPane().add(Date, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 10, 110, 23));
 
         Time.setText("Time");
-        getContentPane().add(Time, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 20, 97, 25));
+        getContentPane().add(Time, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 10, 97, 25));
 
         jLabel1.setFont(new java.awt.Font("Ravie", 1, 18)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(0, 153, 153));
